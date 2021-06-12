f63dd11c1ec08c24cfb4df96cc988ee3f8f3fcbe
jhonkophler <juanadriancorso94@gmail.com>
2021-06-12 17:53:11 -0300

    resguardo desde .io

--- js/actions.js
+++ js/actions.js
@@ -1,68 +1,93 @@
-jQuery( function( $ ) {
-    
-    var $window = $( window ); // 1. Window Object.
-    var $featuredMedia = $( "#featured-media" ); // 1. The Video Container.
-    var $featuredVideo = $( "#featured-video" ); // 2. The Youtube Video.
-    
-    var player; // 3. Youtube player object.
-    var top = $featuredMedia.offset().top; // 4. The video position from the top of the document;
-    var offset = Math.floor( top + ( $featuredMedia.outerHeight() / 2 ) ); //5. offset.
+
+let enlacesHeader = document.querySelectorAll(".menu")[0];
+let semaforo = true;
+
+document.querySelectorAll(".hamburger")[0].addEventListener("click",function(){
+   if(semaforo){
+      document.querySelectorAll(".hamburger>i")[0].style.color="rgb(252,151,0)";
+      semaforo=false;
+   }else{
+      document.querySelectorAll(".hamburger>i")[0].style.color="#fbf7fd";
+      semaforo=true;
+   }
+   
+   
+   enlacesHeader.classList.toggle("menu2")
+})
 
 
 
-    window.onYouTubeIframeAPIReady = function() {
-        player = new YT.Player( "featured-video", {
-           events: {
-             "onStateChange": onPlayerStateChange
-           }
-        } );
-    };
 
-    
-    /**
- * Run when the Youtube video state (play, pause, etc.) is changed.
- *
- * @param {Object} event The Youtube Object Event.
- * @return {Void}
- */
-    function onPlayerStateChange( event ) {
+jQuery(function ($) {
 
-    var isPlay  = 1 === event.data;
-    var isPause = 2 === event.data;
-    var isEnd   = 0 === event.data;
+   var $window = $(window); // 1. Window Object.
+   var $featuredMedia = $("#featured-media"); // 1. The Video Container.
+   var $featuredVideo = $("#featured-video"); // 2. The Youtube Video.
 
-        if ( isPlay ) {
-           $featuredVideo.removeClass( "is-paused" );
-           $featuredVideo.toggleClass( "is-playing" );
-        }
-    
-        if ( isPause ) {
-           $featuredVideo.removeClass( "is-playing" );
-           $featuredVideo.toggleClass( "is-paused" );
-        }
-    
-        if ( isEnd ) {
-           $featuredVideo.removeClass( "is-playing", "is-paused" );
+   var player; // 3. Youtube player object.
+   var top = $featuredMedia.offset().top; // 4. The video position from the top of the document;
+   var offset = Math.floor(top + ($featuredMedia.outerHeight() / 2)); //5. offset.
+
+
+
+   window.onYouTubeIframeAPIReady = function () {
+      player = new YT.Player("featured-video", {
+         events: {
+            "onStateChange": onPlayerStateChange
          }
-    }
+      });
+   };
+
+
+   /**
+* Run when the Youtube video state (play, pause, etc.) is changed.
+*
+* @param {Object} event The Youtube Object Event.
+* @return {Void}
+*/
+   function onPlayerStateChange(event) {
+
+      var isPlay = 1 === event.data;
+      var isPause = 2 === event.data;
+      var isEnd = 0 === event.data;
+
+      if (isPlay) {
+         $featuredVideo.removeClass("is-paused");
+         $featuredVideo.toggleClass("is-playing");
+      }
+
+      if (isPause) {
+         $featuredVideo.removeClass("is-playing");
+         $featuredVideo.toggleClass("is-paused");
+      }
+
+      if (isEnd) {
+         $featuredVideo.removeClass("is-playing", "is-paused");
+      }
+   }
 
 
 
 
-    $window
-    .on( "resize", function() {
-       top = $featuredMedia.offset().top;
-       offset = Math.floor( top + ( $featuredMedia.outerHeight() / 2 ) );
-    } )
-    
-    .on( "scroll", function() {
-       $featuredVideo.toggleClass( "is-sticky",
-         $window.scrollTop() > offset && $featuredVideo.hasClass( "is-playing" )
-       );
-    } 
-    );
+   $window
+      .on("resize", function () {
+         top = $featuredMedia.offset().top;
+         offset = Math.floor(top + ($featuredMedia.outerHeight() / 2));
+      })
 
-    
+      .on("scroll", function () {
+         $featuredVideo.toggleClass("is-sticky",
+            $window.scrollTop() > offset && $featuredVideo.hasClass("is-playing")
+         );
+      }
+      );
 
 
-} );
+
+
+});
+
+
+
+/* ANIMACION DE TALENTO */
+
